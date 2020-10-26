e945a494047e88d4291f09c33ee8f43aad6e24a9
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-26 10:22:50 +0100

    added debugging and requirements

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 <?php
+
+ini_set('display_errors', '1');
+
+ini_set('display_startup_errors', '1');
+
+error_reporting(E_ALL);
+
+require 'View/insert.php';
+require 'Model/Connection.php';
+function whatIsHappening()
+{
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+
+//whatishappening();
