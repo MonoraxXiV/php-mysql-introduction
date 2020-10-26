0b9fc838c8ab230ac8e7b3c090c3b6902b52c7e4
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-26 16:47:44 +0100

    passing on data

--- Controller/HomepageController.php
+++ Controller/HomepageController.php
@@ -3,14 +3,20 @@
 
 class HomepageController
 {
+
+
     public function getInformation(){
         $connector= new Connection();
-        $connector->getPdo();
+        $pdo=$connector->getPdo();
+        $handle=$pdo->query('SELECT * FROM student ');
+        $handle->execute();
+       $students=$handle->fetchAll();
 
-
+        var_dump($students);
+        return $students;
     }
     public function renderHomepage(array $GET, array $POST){
-
+       $students=$this->getInformation();
         require 'View/Homepage.php';
     }
 
