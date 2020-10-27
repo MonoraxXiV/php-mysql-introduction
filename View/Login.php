504757ef1020faa59565bb80afcef55ce0755333
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-27 14:25:28 +0100

    Login page view

---
+++ View/Login.php
@@ -0,0 +1,21 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Log in</title>
+</head>
+<body>
+<form  method="post">
+<label for="email">e-mail:</label><br>
+<input type="text" id="email" name="email"><br>
+<label for="password">password:</label><br>
+<input type="text" id="password" name="password"><br>
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
