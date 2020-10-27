3516dd8d5eda719cca12b6651fef24deb2770dca
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-27 15:55:06 +0100

    defined variables, def rework this if you have time left

--- Controller/InsertController.php
+++ Controller/InsertController.php
@@ -4,70 +4,84 @@ require 'Model/Connection.php';
 
 class InsertController
 {
-    public function inputStudentData(){
 
+public $fNameErr;
+public $lNameErr;
+public $emailErr;
+public $passErr ;
+public $passConfErr = "";
 
-    }
+public $firstName;
+public $lastName;
+public $email;
+public $password;
+public $passConfirm;
 
-    public function renderInsert(array $GET, array $POST)
+    public function inputStudentData()
     {
-        $firstName = $lastName = $email = "";
-        $fNameErr = $lNameErr = $emailErr = $passErr= $passConfErr="";
+
+
         $date = new DateTime();
         $id = 0;
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (empty($_POST['fname'])) {
-                $fNameErr = "First name is required" . '<br>';
+                $this->fNameErr = "First name is required" . '<br>';
             } else {
-                $firstName = $_POST['fname'];
+                $this->firstName = $_POST['fname'];
             }
             if (empty($_POST['lname'])) {
-                $lNameErr = "Last name is required" . '<br>';
+                $this->lNameErr = "Last name is required" . '<br>';
             } else {
-                $lastName = $_POST['lname'];
+                $this->lastName = $_POST['lname'];
             }
 
             if (isset($_POST['email'])) {
-                $email = $_POST['email'];
-                filter_var($email, FILTER_VALIDATE_EMAIL);
+                $this->email = $_POST['email'];
+                filter_var($this->email, FILTER_VALIDATE_EMAIL);
             } else {
 
-                $emailErr = "E-mail adress is invalid" . '<br>';
+                $this->emailErr = "E-mail adress is invalid" . '<br>';
             }
 
             if (empty($_POST['password'])) {
-                $passErr = "password is required" . '<br>';
+                $this->passErr = "password is required" . '<br>';
             } else {
-                $password=$_POST['password'];
+                $this->password = $_POST['password'];
             }
             if (empty($_POST['passwordConf'])) {
-                $passConfErr = "please confirm the password" . '<br>';
-            }else {
-                $passConfirm=$_POST['passwordConf'];
+                $this->passConfErr = "please confirm the password" . '<br>';
+            } else {
+                $this->passConfirm = $_POST['passwordConf'];
             }
 
-            if ($password !== $passConfirm){
-                $passConfErr=" passwords do not match". '<br>';
+            if ($this->password !== $this->passConfirm) {
+                $this->passConfErr = " passwords do not match" . '<br>';
             }
-            if ($fNameErr == "" && $lNameErr == "" && $emailErr == "" && $passConfErr== "" && $passErr =="") {
+            if ($this->fNameErr == "" && $this->lNameErr == "" && $this->emailErr == "" && $this->passConfErr == "" && $this->passErr == "") {
                 //Post code to database.
                 $connector = new Connection();
                 $pdo = $connector->getPdo();
 
                 $handle = $pdo->prepare("INSERT INTO student VALUES (:id, :first_name, :last_name, :email, :created_at )");
-                $handle->bindValue(':first_name', $firstName);
-                $handle->bindValue(':last_name', $lastName);
-                $handle->bindValue(':email', $email);
+                $handle->bindValue(':first_name', $this->firstName);
+                $handle->bindValue(':last_name', $this->lastName);
+                $handle->bindValue(':email', $this->email);
                 $handle->bindValue(':created_at', $date->format('Y-m-d H:i:s'));
                 $handle->bindValue(':id', $id);
                 $handle->execute();
             }
-
-
-            var_dump($POST);
-            require 'View/register.php';
         }
+    }
 
+    public
+    function renderInsert(array $GET, array $POST)
+    {
 
+        $this->inputStudentData();
+        var_dump($POST);
+        require 'View/register.php';
     }
 }
+
+
+
