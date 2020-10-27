6736d90faea3190c07fa53dbcb16ab6418915171
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-27 15:50:40 +0100

    bugs resolved by making values known in insertController, def remake a bit if you have time left

--- View/register.php
+++ View/register.php
@@ -13,22 +13,22 @@
 <form  method="post">
     <h1>Registration form</h1>
     <label for="fname">First name:</label><br>
-    <input type="text" id="fname" name="fname"  value="<?php echo $firstName ?>"><br>
-    <span class="error"><?php echo $fNameErr;?></span>
+    <input type="text" id="fname" name="fname"  value="<?php echo $this->firstName ?>"><br>
+    <span class="error"><?php echo $this->fNameErr;?></span>
 
     <label for="lname">Last name:</label><br>
-    <input type="text" id="lname" name="lname"  value="<?php echo $lastName ?>"><br>
-    <span class="error"><?php echo $lNameErr;?></span>
+    <input type="text" id="lname" name="lname"  value="<?php echo $this->lastName ?>"><br>
+    <span class="error"><?php echo $this->lNameErr;?></span>
     <label for="email">e-mail:</label><br>
-    <input type="text" id="email" name="email" value="<?php echo $email ?>"><br>
-    <span class="error"><?php echo $emailErr;?></span>
+    <input type="text" id="email" name="email" value="<?php echo $this->email ?>"><br>
+    <span class="error"><?php echo $this->emailErr;?></span>
 
     <label for="password">password:</label><br>
     <input type="text" id="password" name="password"><br>
-    <span class="error"><?php echo $passErr;?></span>
+    <span class="error"><?php echo $this->passErr;?></span>
     <label for="password">confirm password:</label><br>
     <input type="text" id="passwordConf" name="passwordConf"><br>
-    <span class="error"><?php echo $passConfErr;?></span>
+    <span class="error"><?php echo $this->passConfErr;?></span>
     <input type="submit" value="Submit">
 
 
