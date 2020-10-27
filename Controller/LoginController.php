45cda9e559fa255faa27ebc77645d20ce984c008
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-27 15:00:19 +0100

    start of log in logic

---
+++ Controller/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class LoginController
+{
+    public function getLogin(array $GET, array $POST){
+
+        $passErr=$emailErr="";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($_POST['email'])) {
+                $emailErr = " e-mail is required" . '<br>';
+            }else {
+                $email=$_POST['email'];
+            }
+
+            if (empty($_POST['password'])) {
+                $passErr = " password is required" . '<br>';
+            }else {
+                $password=$_POST['password'];
+            }
+
+
+        }
+
+    }
+
+    public function LoginRender(array $GET, array $POST){
+
+        $this->getLogin($GET, $POST);
+        var_dump($_POST);
+        require 'View/Login.php';
+    }
+}
