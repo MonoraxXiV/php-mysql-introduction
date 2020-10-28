c1e0b48d0a62ec3a3c8b7c7e8120b5584a4f749d
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-28 16:57:56 +0100

    attempting to pass boolean to index

--- Controller/LoginController.php
+++ Controller/LoginController.php
@@ -1,25 +1,32 @@
 <?php
 
 
+
 class LoginController
 {
+    public $passErr;
+    public $emailErr;
     public function getLogin(array $GET, array $POST){
 
-        $passErr=$emailErr="";
+        $this->passErr=$this->emailErr="";
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if (empty($_POST['email'])) {
-                $emailErr = " e-mail is required" . '<br>';
-            }else {
+                $this->emailErr = " e-mail is required" . '<br>';
+            }elseif(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+                $this->emailErr= "e-mail is invalid";
+            }
+            else{
                 $email=$_POST['email'];
             }
 
             if (empty($_POST['password'])) {
-                $passErr = " password is required" . '<br>';
+                $this->passErr = " password is required" . '<br>';
             }else {
                 $password=$_POST['password'];
             }
             $Auth=new Auth();
             $isLoggedIn=$Auth->getLogin($email,$password);
+            return $isLoggedIn;
 
         }
 
