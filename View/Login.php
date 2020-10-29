c9abc830150e07b1a86a3569142802d66c26a69f
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-10-29 16:39:34 +0100

    added comments for future improvements

--- View/Login.php
+++ View/Login.php
@@ -14,11 +14,14 @@
 <form  method="post">
 <label for="email">e-mail:</label><br>
 <input type="text" id="email" name="email"><br>
-    <span class="error"><?php echo $emailErr;?></span>
+    <span class="error"><?php echo $this->emailErr;?></span>
 <label for="password">password:</label><br>
+    <!-- need to find a way to not show previously entered words into the password box and to display as dots -->
 <input type="text" id="password" name="password"><br>
-    <span class="error"><?php echo $passErr;?></span>
+    <span class="error"><?php echo $this->passErr;?></span>
 <input type="submit" value="Submit">
+    <!-- put in a link that actually works or put it in the if statements when switching -->
+    <p>don't have an account yet? <a href="register.php">Register here</a></p>
 </form>
 </body>
 </html>
